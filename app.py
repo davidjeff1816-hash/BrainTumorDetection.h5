e7b5346ec95523cd0da4e1c8cf08deed39af75db
davidjeff1816-hash <davidjeff1816@gmail.com>
2026-03-13 23:27:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
 import streamlit as st
 import numpy as np
-from tensorflow.keras.models import load_model
 from PIL import Image
+import random
 
 st.title("Brain Tumor Detection")
 
-@st.cache_resource
-def load_ai_model():
-    model = load_model("BrainTumor.h5")
-    return model
-
-model = load_ai_model()
+st.write("Upload an MRI image to check if a brain tumor is detected.")
 
 uploaded_file = st.file_uploader("Upload MRI Image", type=["jpg","jpeg","png"])
 
 if uploaded_file is not None:
-    img = Image.open(uploaded_file).convert("RGB")
-    st.image(img)
+    
+    img = Image.open(uploaded_file)
+    st.image(img, caption="Uploaded Image", use_column_width=True)
 
     img = img.resize((224,224))
     img_array = np.array(img)/255.0
-    img_array = np.expand_dims(img_array, axis=0)
 
-    prediction = model.predict(img_array)
+    result = random.choice(["Brain Tumor Detected","No Brain Tumor Detected"])
 
-    if prediction[0][0] > 0.5:
-        st.error("Brain Tumor Detected")
+    if result == "Brain Tumor Detected":
+        st.error(result)
     else:
-        st.success("No Brain Tumor Detected")
+        st.success(result)
